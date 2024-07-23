92441a003d3f47d3cc2d7fd702ef87d9a4c557a9
0219angry <angry.yuma.0219@gmail.com>
2024-07-23 16:13:54 +0900

    WIP

--- internal/clog/clog.go
+++ internal/clog/clog.go
@@ -1,5 +1,20 @@
 package clog
 
-func InitLogger(packegeName string) {
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"time"
+)
 
+var logger *slog.Logger
+
+func InitLogger() *slog.Logger {
+	currentTime := time.Now().Format("2006-01-02_15:04:05")
+	logFileName := fmt.Sprintf("%s%s", "./log/logfile_", currentTime)
+
+	f, _ := os.Create(logFileName)
+	logger = slog.New(slog.NewJSONHandler(f, nil))
+
+	return logger
 }
